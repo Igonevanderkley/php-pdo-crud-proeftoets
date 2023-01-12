63b029e7d20a4692c4a3b2389104d61559cd14c0
Igone van_der_Kley <igone@vdkley.nl>
2023-01-12 11:50:00 +0100

    derde commit 11:49

--- read.php
+++ read.php
@@ -17,13 +17,56 @@ echo $e->$getMessage();
 }
 
 //haal gegevens uit de database
-$sql = "SELECT * FROM RichestPeople";
+$sql = "SELECT * FROM RichestPeople
+ORDER BY Networth desc";
 
 $statement = $pdo->prepare($sql); 
-$statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_OBJ);
 
-var_dump($result);
+$statement->execute();
+
+$result = $statement->fetchAll(PDO::FETCH_OBJ);
+//ff checken
+//var_dump($result);
+
+
+$rows = "";
+foreach ($result as $info) {
+    $rows .= "<tr>
+                <td>$info->Name</td>
+                <td>$info->Networth</td>
+                <td>$info->Age</td>
+                <td>$info->MyCompany</td>
+                <tr>      
+     ";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <br>
+    <br>
+    <table border='1'>
+    <thead>
+        <th>Naam</th>
+        <th>Vermogen</th>
+        <th>Leeftijd</th>
+        <th>Bedrijf</th>
+    <t/head>
+    <tbody>
+        <?= $rows ?>
+        </tbody>
+    </table> 
+</body>
+</html>
+
 
 
 
